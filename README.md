b8b091833ff4587694a49c7708bc3c0b8f2f08b0
robmoss2k <15984835+robmoss2k@users.noreply.github.com>
2021-03-09 11:40:38 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# crossover-worksmart
-Crossover Worksmart
+﻿# ![Crossover WorkSmart](https://www.crossover.com/assets/img/favicon.png "Crossover WorkSmart") [Crossover WorkSmart](https://www.crossover.com/pages/worksmart-productivity-tool)
+
+WorkSmart is Crossover's productivity tool, used by its contractors to track
+time and activity.
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://s3.amazonaws.com/crossover-clients/crossovertool.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  softwareName   = 'Crossover WorkSmart*'
+  checksum       = 'F7FC7B2881C3C68F204C04317E934AE2ED5E485D1B5CB7338820610A0E9305EB'
+  checksumType   = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/s /v"/qn"'
+}
+
+Install-ChocolateyPackage @packageArgs
